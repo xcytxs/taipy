b26ea1689da03a885425c218fd86cb9b482a0d37
namnguyen <namnguyen20999@gmail.com>
2024-11-05 13:17:30 +0700

    per Fab

--- doc/gui/extension/example_library/front-end/src/index.ts
+++ doc/gui/extension/example_library/front-end/src/index.ts
@@ -7,6 +7,6 @@
 // Note that we export the 'ColoredLabel' component as 'ExampleLabel', which is
 // the name used in the element declaration in the element library.
 import ColoredLabel from "./ColoredLabel";
-import GameTable from "./BasicTable";
+import GameTable from "./GameTable";
 
 export { ColoredLabel as ExampleLabel, GameTable };
