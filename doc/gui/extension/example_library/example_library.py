4ab15260d4319e3bd1adecec699ac845b97b4fb1
namnguyen <namnguyen20999@gmail.com>
2024-12-03 14:36:21 +0700

    ruff

--- doc/gui/extension/example_library/example_library.py
+++ doc/gui/extension/example_library/example_library.py
@@ -9,8 +9,9 @@
 # an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
 # specific language governing permissions and limitations under the License.
 
-from taipy.gui.extension import Element, ElementLibrary, ElementProperty, PropertyType
 import base64
+
+from taipy.gui.extension import Element, ElementLibrary, ElementProperty, PropertyType
 
 
 class ExampleLibrary(ElementLibrary):
