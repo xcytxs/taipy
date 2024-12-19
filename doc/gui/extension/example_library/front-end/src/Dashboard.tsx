e719b7c20e05dc4b8c2a19c936f303233026006b
namnguyen <namnguyen20999@gmail.com>
2024-12-19 14:48:53 +0700

    remove <div>

--- doc/gui/extension/example_library/front-end/src/Dashboard.tsx
+++ doc/gui/extension/example_library/front-end/src/Dashboard.tsx
@@ -12,41 +12,37 @@
  */
 
 import React, { useMemo } from "react";
-import { useDynamicJsonProperty, } from "taipy-gui";
+import { useDynamicJsonProperty } from "taipy-gui";
 
 import Plot from "react-plotly.js";
 import { Data, Layout } from "plotly.js";
 
 interface DashboardProps {
-    data?: string;
-    defaultData?: string;
-    layout?: string;
-    defaultLayout?: string;
+  data?: string;
+  defaultData?: string;
+  layout?: string;
+  defaultLayout?: string;
 }
 
 const Dashboard = (props: DashboardProps) => {
-    const value = useDynamicJsonProperty(props.data, props.defaultData || "", {} as Partial<Data>);
-    const dashboardLayout = useDynamicJsonProperty(props.layout, props.defaultLayout || "", {} as Partial<Layout>);
+  const value = useDynamicJsonProperty(props.data, props.defaultData || "", {} as Partial<Data>);
+  const dashboardLayout = useDynamicJsonProperty(props.layout, props.defaultLayout || "", {} as Partial<Layout>);
 
-    const data = useMemo(() => {
-        if (Array.isArray(value)) {
-            return value as Data[];
-        }
-        return [] as Data[];
-    }, [value]);
+  const data = useMemo(() => {
+    if (Array.isArray(value)) {
+      return value as Data[];
+    }
+    return [] as Data[];
+  }, [value]);
 
-    const baseLayout = useMemo(() => {
-        const layout = {
-            ...dashboardLayout,
-        };
-        return layout as Partial<Layout>;
-    }, [dashboardLayout]);
+  const baseLayout = useMemo(() => {
+    const layout = {
+      ...dashboardLayout,
+    };
+    return layout as Partial<Layout>;
+  }, [dashboardLayout]);
 
-    return (
-        <div>
-            <Plot data={data} layout={baseLayout} />
-        </div>
-    );
+  return <Plot data={data} layout={baseLayout} />;
 };
 
 export default Dashboard;
